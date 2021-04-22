e93053fcc666cf6619377a9af509e797eb028ad4
ColtxZZ <82120154+ColtxZZ@users.noreply.github.com>
2021-04-22 10:26:29 -0300

    Update MortgageCalculator.java

--- src/main/java/com/h2/MortgageCalculator.java
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -6,7 +6,7 @@ public class MortgageCalculator {
     private long loanAmount;
     private int termInYears;
     private float annualRate;
-    private double monthLyPayment;
+    private double monthlyPayment;
 
     public MortgageCalculator(long loanAmount, int termInYears, float annualRate) {
         this.loanAmount = loanAmount;
@@ -14,7 +14,7 @@ public class MortgageCalculator {
         this.annualRate = annualRate;
     }
 
-    private int getNumbersOfPayments() {
+    private int getNumberOfPayments() {
         return termInYears * 12;
     }
 
@@ -26,15 +26,15 @@ public class MortgageCalculator {
     public void calculateMonthlyPayment() {
         long P = loanAmount;
         float r = getMonthlyInterestRate();
-        int n = getNumbersOfPayments();
+        int n = getNumberOfPayments();
 
         double M = P * (((r * Math.pow(1 + r, n))) / ((Math.pow((1 + r), n)) - 1));
-        this.monthLyPayment = M;
+        this.monthlyPayment = M;
     }
 
     public String toString() {
         DecimalFormat df = new DecimalFormat("####0.00");
-        return "monthlyPayment: " + df.format(monthLyPayment);
+        return "monthlyPayment: " + df.format(monthlyPayment);
     }
 
     public static void main(String[] args) {
